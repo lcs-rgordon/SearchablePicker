dcdc41407b5c666b6b7c72801c65842f3028e03b
Russell Gordon <github@russellgordon.ca>
2022-05-07 09:30:12 -0400

    Quick proof of concept. In a proper project the Province structure should be in it's own file in a group named "Model".

--- SearchablePicker/ContentView.swift
+++ SearchablePicker/ContentView.swift
@@ -7,10 +7,71 @@
 
 import SwiftUI
 
+struct Province: Hashable {
+    let name: String
+    let combinedTaxRate: Double
+    let taxDescription: String
+}
+
 struct ContentView: View {
+    
+    // MARK: Stored properties
+    
+    // What province was selected
+    @State var pickerSelection: Province = Province(name: "Ontario",
+                                                    combinedTaxRate: 0.13,
+                                                    taxDescription: "HST")
+    
+    // List of provinces
+    @State var provinces = [
+        Province(name: "Alberta",
+                 combinedTaxRate: 0.05,
+                 taxDescription: "GST")
+        ,
+        Province(name: "Ontario",
+                 combinedTaxRate: 0.13,
+                 taxDescription: "HST")
+        ,
+        Province(name: "Quebec",
+                 combinedTaxRate: 0.14975,
+                 taxDescription: "QST+GST")
+        ,
+
+    ]
+    
+    
+    // MARK: Computed properties
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            Form {
+                
+                Section(content: {
+                    Picker(selection: $pickerSelection,
+                           label: Text("Province"),
+                           content: {
+
+                        ForEach(provinces, id: \.self) { currentProvince in
+                            Text(currentProvince.name).tag(currentProvince)
+                        }
+                        
+                    })
+                    .pickerStyle(.automatic)
+                }, header: {
+                    Text("Select Jurisdiction")
+                })
+                
+                Section(content: {
+                    Text("\(pickerSelection.taxDescription): \(pickerSelection.combinedTaxRate * 100) %")
+                }, header: {
+                    Text("Applied Tax Rate")
+                })
+                
+                
+            }
+            .navigationTitle("Tax Calculator")
+            
+        }
     }
 }
 
