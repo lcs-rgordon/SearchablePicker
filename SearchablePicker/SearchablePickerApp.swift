e7e7a19824d965ae731f7ecc00cbc53d970fa741
Russell Gordon <github@russellgordon.ca>
2022-05-07 08:57:49 -0400

    Initial Commit

---
+++ SearchablePicker/SearchablePickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchablePickerApp.swift
+//  SearchablePicker
+//
+//  Created by Russell Gordon on 2022-05-07.
+//
+
+import SwiftUI
+
+@main
+struct SearchablePickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
